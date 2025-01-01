908c22604ffdb5648a9959fc2358e1f92de9d92f
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2025-01-02 01:57:07 +0300

    authenticated routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\UserController;
@@ -13,39 +13,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Admin routes
+    Route::middleware(['role:admin'])->group(function () {
+        Route::resource('roles', RoleController::class)->except('show');
+        Route::get('/roles/{roleId}/permissions', [RoleController::class, 'showAssignPermissionForm'])->name('roles.permissions');
+        Route::post('/roles/{roleId}/assign-permission/{permissionId}', [RoleController::class, 'assignPermission'])->name('roles.assign-permission');
+        Route::post('/roles/{roleId}/revoke-permission/{permissionId}', [RoleController::class, 'removePermission'])->name('roles.revoke-permission');
+
+        Route::resource('permissions', PermissionController::class)->except('show');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/{userId}/roles', [UserController::class, 'showUserRoles'])->name('users.showUserRoles');
+        Route::put('/users/{userId}/roles', [UserController::class, 'updateRoles'])->name('users.updateRoles');
+    });
+
+    // Shop routes
+    Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
+
+    // Product routes
+    Route::get('/shops/{shopId}/products', [ProductController::class, 'getShopsProducts'])->name('products.getShopsProducts');
+
+    // Order routes
+    Route::post('/orders', [OrderController::class, 'createOrder'])->name('orders.createOrder');
 });
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    // roles routes
-    Route::resource('roles', RoleController::class)->except('show');
-    Route::get('/roles/{roleId}/permissions', [RoleController::class, 'showAssignPermissionForm'])->name('roles.permissions');
-    Route::post('/roles/{roleId}/assign-permission/{permissionId}', [RoleController::class, 'assignPermission'])->name('roles.assign-permission');
-    Route::post('/roles/{roleId}/revoke-permission/{permissionId}', [RoleController::class, 'removePermission'])->name('roles.revoke-permission');
-
-    // permissions routes
-    Route::resource('permissions', PermissionController::class)->except('show');
-
-    // users routes
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{userId}/roles', [UserController::class, 'showUserRoles'])->name('users.showUserRoles');
-    Route::put('/users/{userId}/roles', [UserController::class, 'updateRoles'])->name('users.updateRoles');
-});
-
-// shop routes
-Route::get('/shops', [ShopController::class, 'index'])->name('shops.index');
-
-// product routes
-Route::get('/shops/{shopId}/products', [ProductController::class, 'getShopsProducts'])->name('products.getShopsProducts');
-
-// order routes
-Route::post('/orders', [OrderController::class, 'createOrder'])->name('orders.createOrder');
 
 require __DIR__ . '/auth.php';
