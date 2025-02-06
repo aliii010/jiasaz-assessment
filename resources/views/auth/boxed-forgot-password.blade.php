f6cef719df3532908773b0aa5695f3e458e398be
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2025-02-06 17:27:58 +0300

    forgot and reset password

--- resources/views/auth/boxed-forgot-password.blade.php
+++ resources/views/auth/boxed-forgot-password.blade.php
@@ -54,24 +54,19 @@
                         </div>
                     </div>
                     <div class="mx-auto w-full max-w-[440px]">
-                        <div class="mb-10">
-                            <h1 class="text-3xl font-extrabold uppercase !leading-snug text-primary md:text-4xl">Sign in
-                            </h1>
-                            <p class="text-base font-bold leading-normal text-white-dark">Enter your email and new
-                                password
-                                to reset your password</p>
+                        <div class="mb-7">
+                            <h1 class="mb-3 text-2xl font-bold !leading-snug dark:text-white">Password Reset</h1>
+                            <p class="mb-3">Forgot your password? No problem. Just let us know your email address and
+                                we will email
+                                you a password reset link that will allow you to choose a new one.</p>
                         </div>
-                        <form method="POST" action="{{ route('password.store') }}" class="space-y-5 dark:text-white">
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+                        <form method="POST" action="{{ route('password.email') }}" class="space-y-5">
                             @csrf
-
-                            <!-- Password Reset Token -->
-                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
                             <div>
-                                <label for="Email">Email</label>
+                                <label for="Email" class="dark:text-white">Email</label>
                                 <div class="relative text-white-dark">
                                     <input id="Email" type="email" placeholder="Enter Email" name="email"
-                                        :value="old('email', $request - > email)"
                                         class="form-input ps-10 placeholder:text-white-dark" />
                                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                     <span class="absolute start-4 top-1/2 -translate-y-1/2">
@@ -86,64 +81,9 @@
                                     </span>
                                 </div>
                             </div>
-                            <div>
-                                <label for="Password">Password</label>
-                                <div class="relative text-white-dark">
-                                    <input id="Password" type="password" placeholder="Enter Password" name="password"
-                                        class="form-input ps-10 placeholder:text-white-dark" />
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                    <span class="absolute start-4 top-1/2 -translate-y-1/2">
-                                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
-                                            <path opacity="0.5"
-                                                d="M1.5 12C1.5 9.87868 1.5 8.81802 2.15901 8.15901C2.81802 7.5 3.87868 7.5 6 7.5H12C14.1213 7.5 15.182 7.5 15.841 8.15901C16.5 8.81802 16.5 9.87868 16.5 12C16.5 14.1213 16.5 15.182 15.841 15.841C15.182 16.5 14.1213 16.5 12 16.5H6C3.87868 16.5 2.81802 16.5 2.15901 15.841C1.5 15.182 1.5 14.1213 1.5 12Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M6 12.75C6.41421 12.75 6.75 12.4142 6.75 12C6.75 11.5858 6.41421 11.25 6 11.25C5.58579 11.25 5.25 11.5858 5.25 12C5.25 12.4142 5.58579 12.75 6 12.75Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M9 12.75C9.41421 12.75 9.75 12.4142 9.75 12C9.75 11.5858 9.41421 11.25 9 11.25C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M12.75 12C12.75 12.4142 12.4142 12.75 12 12.75C11.5858 12.75 11.25 12.4142 11.25 12C11.25 11.5858 11.5858 11.25 12 11.25C12.4142 11.25 12.75 11.5858 12.75 12Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M5.0625 6C5.0625 3.82538 6.82538 2.0625 9 2.0625C11.1746 2.0625 12.9375 3.82538 12.9375 6V7.50268C13.363 7.50665 13.7351 7.51651 14.0625 7.54096V6C14.0625 3.20406 11.7959 0.9375 9 0.9375C6.20406 0.9375 3.9375 3.20406 3.9375 6V7.54096C4.26488 7.51651 4.63698 7.50665 5.0625 7.50268V6Z"
-                                                fill="currentColor" />
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div>
-                            <div>
-                                <label for="Password">Password Confirmation</label>
-                                <div class="relative text-white-dark">
-                                    <input id="Password" type="password" placeholder="Enter Password"
-                                        name="password_confirmation"
-                                        class="form-input ps-10 placeholder:text-white-dark" />
-                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                                    <span class="absolute start-4 top-1/2 -translate-y-1/2">
-                                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
-                                            <path opacity="0.5"
-                                                d="M1.5 12C1.5 9.87868 1.5 8.81802 2.15901 8.15901C2.81802 7.5 3.87868 7.5 6 7.5H12C14.1213 7.5 15.182 7.5 15.841 8.15901C16.5 8.81802 16.5 9.87868 16.5 12C16.5 14.1213 16.5 15.182 15.841 15.841C15.182 16.5 14.1213 16.5 12 16.5H6C3.87868 16.5 2.81802 16.5 2.15901 15.841C1.5 15.182 1.5 14.1213 1.5 12Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M6 12.75C6.41421 12.75 6.75 12.4142 6.75 12C6.75 11.5858 6.41421 11.25 6 11.25C5.58579 11.25 5.25 11.5858 5.25 12C5.25 12.4142 5.58579 12.75 6 12.75Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M9 12.75C9.41421 12.75 9.75 12.4142 9.75 12C9.75 11.5858 9.41421 11.25 9 11.25C8.58579 11.25 8.25 11.5858 8.25 12C8.25 12.4142 8.58579 12.75 9 12.75Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M12.75 12C12.75 12.4142 12.4142 12.75 12 12.75C11.5858 12.75 11.25 12.4142 11.25 12C11.25 11.5858 11.5858 11.25 12 11.25C12.4142 11.25 12.75 11.5858 12.75 12Z"
-                                                fill="currentColor" />
-                                            <path
-                                                d="M5.0625 6C5.0625 3.82538 6.82538 2.0625 9 2.0625C11.1746 2.0625 12.9375 3.82538 12.9375 6V7.50268C13.363 7.50665 13.7351 7.51651 14.0625 7.54096V6C14.0625 3.20406 11.7959 0.9375 9 0.9375C6.20406 0.9375 3.9375 3.20406 3.9375 6V7.54096C4.26488 7.51651 4.63698 7.50665 5.0625 7.50268V6Z"
-                                                fill="currentColor" />
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div>
                             <button type="submit"
                                 class="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
-                                Reset Password </button>
+                                Email Password Reset Link </button>
                         </form>
                     </div>
                 </div>
@@ -239,66 +179,4 @@
         });
     </script>
 
-</x-layout.auth>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<x-layout.auth>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
-
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)"
-                required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-layout.auth>
